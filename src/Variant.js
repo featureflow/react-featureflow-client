fdffa345be1983939fa48defa16871f4b0b00f5b
Henry Young <henry.young@industrieit.com>
2017-03-31 16:36:13 +1100

    Varaint to allow multiple children

--- src/Variant.js
+++ src/Variant.js
@@ -3,11 +3,6 @@ import React, { Component, PropTypes, Children } from 'react';
 
 export default class Variant extends Component{
   render(){
-    return Children.only(this.props.children);
+    return this.props.children;
   }
 }
-
-Variant.propTypes = {
-  children: PropTypes.element.isRequired
-};
-
