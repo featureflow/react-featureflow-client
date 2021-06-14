668e10bfd7806475c4ca2e2e78cd5bc78231d7be
Oliver Oldfield-Hodge <oliver@featureflow.io>
2021-06-15 09:58:46 +1000

    Tidy Up

--- src/withFeatureflow.js
+++ src/withFeatureflow.js
@@ -1,5 +1,5 @@
 //
-import React, { Component } from 'react';
+import React from 'react';
 import {Consumer} from "./context";
 
 
@@ -29,75 +29,5 @@ export default function(featureflowConfig: ? FeatureflowConfig = {}){
           }}
         </Consumer>
     )
-    /*class WithFeatureflowClient extends Component{
-      _handleUpdated;
-      state;
-      evaluated: {};
-      config;
-
-      constructor(props, context){
-        debugger;
-        
-
-        super(props, context);
-
-        this.config = {
-          ...this.context.featureflowConfig,
-          ...featureflowConfig
-        };
-
-        this.state = this.context.featureflowClient.getFeatures();
-        this.evaluated = {};
-
-        this._handleUpdated = this.handleUpdated.bind(this);
-
-        this.context.featureflowClient.on('INIT', this._handleUpdated);
-
-        if (this.config.update){
-          this.context.featureflowClient.on('UPDATED_FEATURE', this._handleUpdated);
-        }
-      }
-
-      componentWillUnmount(){
-        if (this._handleUpdated){
-          this.context.featureflowClient.off('UPDATED_FEATURE', this._handleUpdated);
-          this.context.featureflowClient.off('INIT', this._handleUpdated);
-        }
-      }
-
-      handleUpdated(){
-        this.evaluated = {};
-        this.setState(this.context.featureflowClient.getFeatures());
-      }
-
-      //Caching evaluate to not send events every render
-      evaluate(feature: string){
-        if (this.evaluated[feature] === undefined){
-          this.evaluated[feature] = this.context.featureflowClient.evaluate(feature)
-        }
-        return this.evaluated[feature];
-      }
-      goal(goalKey: string){
-        return this.context.featureflowClient.goal(goalKey);
-      }
-      render(){
-
-        if (this.config.waitForInit && !this.context.featureflowClient.hasReceivedInitialResponse()){
-          return this.config.preInitComponent || <div></div>
-        }
-        return React.createElement(WrappedComponent, {[this.config.clientName]: {
-          ...this.context.featureflowClient,
-          evaluate: this.evaluate.bind(this),
-          goal: this.goal.bind(this),
-        }, ...this.props});
-      }
-    }
-
-    WithFeatureflowClient.contextTypes = {
-      featureflowClient: featureflowClientShape.isRequired,
-      featureflowConfig: featureflowConfigShape.isRequired
-      
-    };
-    return WithFeatureflowClient;*/
   }
 }
